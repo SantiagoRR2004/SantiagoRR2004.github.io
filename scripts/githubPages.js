3428d202e007fea0cc2c68f89cb592b76f60e81b
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2026-01-10 18:58:22 +0100

    Use customRepos

--- scripts/githubPages.js
+++ scripts/githubPages.js
@@ -1,32 +1,30 @@
 appReady.then(() => {
   const pagesList = document.getElementById("pagesList");
+  pagesList.innerHTML = "";
 
-  fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
-    .then((response) => response.json())
-    .then((repos) => {
-      pagesList.innerHTML = "";
-      repos.forEach((repo) => {
-        if (repo.name.toLowerCase() !== `${username}.github.io`.toLowerCase()) {
-          const pagesUrl = `https://${username}.github.io/${repo.name}/`;
+  Object.keys(customRepos).forEach((repoUrl) => {
+    const repoName = repoUrl.split("/").slice(-1)[0];
+    if (repoName.toLowerCase() === `${username}.github.io`.toLowerCase()) {
+      // This is the main GitHub Pages repo, skip it
+      return;
+    }
+    const owner = repoUrl.split("/").at(-2);
+    const pagesUrl = `https://${owner}.github.io/${repoName}/`;
 
-          // Check if the GitHub Pages URL exists
-          fetch(pagesUrl, { method: "HEAD" }) // Use HEAD to check without downloading full content
-            .then((res) => {
-              if (res.ok) {
-                // not a 404
-                const li = document.createElement("li");
-                li.innerHTML = `<a href="${pagesUrl}" target="_blank">${repo.name}</a>`;
-                pagesList.appendChild(li);
-              }
-            })
-            .catch((err) => {
-              console.warn(`Could not check ${pagesUrl}`, err);
-            });
+    // Check if the GitHub Pages URL exists
+    fetch(pagesUrl, { method: "HEAD" }) // Use HEAD to check without downloading full content
+      .then((res) => {
+        if (res.ok) {
+          // not a 404
+          const li = document.createElement("li");
+          li.innerHTML = `<a href="${pagesUrl}" target="_blank">${repoName}</a>`;
+          pagesList.appendChild(li);
+        } else {
+          console.log(`Could not find deployment at ${pagesUrl}`);
         }
+      })
+      .catch((err) => {
+        console.log(`Could not find deployment at ${pagesUrl}`);
       });
-    })
-    .catch((error) => {
-      pagesList.innerHTML = "Failed to load repositories.";
-      console.error(error);
-    });
+  });
 });
